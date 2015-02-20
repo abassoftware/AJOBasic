52b11e02439bb2a33dc0e83ca20cf391fdea3691
Jasmin Scheffel <jasc@abas.de>
2015-02-20 17:50:46 +0100

    Tests for standard selection examples

---
+++ test/de/abas/training/basic/standardselection/CustomerSimpleSelectionTest.java
@@ -0,0 +1,35 @@
+package de.abas.training.basic.standardselection;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import de.abas.training.basic.testutility.TestLogger;
+
+public class CustomerSimpleSelectionTest {
+
+	@Rule
+	public TestName testName = new TestName();
+	private CustomerSimpleSelection instance;
+	private static Logger logger = TestLogger.getLogger();
+
+	@Test
+	public void queryShouldNotBeEmpty() throws Exception {
+		logger.info(String.format(TestLogger.TEST_INIT_MESSAGE,
+				testName.getMethodName()));
+		int status = instance.run(null);
+		assertEquals("status of selection", 0, status);
+		logger.info("query was " + (status == 0 ? "not empty" : "empty"));
+	}
+
+	@Before
+	public void setup() {
+		logger.info(TestLogger.SETUP_MESSAGE);
+		instance = new CustomerSimpleSelection();
+	}
+
+}
